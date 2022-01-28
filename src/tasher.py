1f809658a4777b0cdd63b8bf778643419ffa3c15
Vitalii Morskyi <vitalij20358@gmail.com>
2022-01-28 10:14:55 +0100

    Add folder reader

--- src/tasher.py
+++ src/tasher.py
@@ -1,2 +1,29 @@
 import argparse
 import logging
+from config import config
+from logging_setup import initialize_logging
+from tasks import Dispatcher
+
+initialize_logging()
+parent_parser = argparse.ArgumentParser(add_help=False)
+parent_parser.add_argument('-u', '--user',
+                           default="xxx",
+                           help='username')
+
+main_parser = argparse.ArgumentParser()
+command_subparsers = main_parser.add_subparsers(title="commands", dest="cmd")
+add_parser = command_subparsers.add_parser("add",
+                                           help="add tasks to the database",
+                                           parents=[parent_parser])
+add_parser.add_argument("path", help="folder or file to add", type=str)
+add_parser.add_argument("-d", "--task-details",
+                        help="CSV file with details about each task",
+                        dest="details_csv")
+args = main_parser.parse_args()
+
+dp = Dispatcher()
+match args.cmd:
+    case "add":
+        dp.add_tasks(path=args.path, details_csv=args.details_csv)
+    case _:
+        pass
--- src/tasks.py
+++ src/tasks.py
@@ -1,3 +1,15 @@
+import os
+import glob
+import pandas as pd
+
+SUPPORTED_FILETYPES = [".png", ".jpg", ".jpeg"]
+
+
+def add_file(filename: str):
+
+    pass
+
+
 class Subject:
     subject_name: str
     subject_folder: str
@@ -29,4 +41,24 @@ class Task:
         self.task_tex = task_tex
         self.solution_path = solution
         self.difficulty = difficulty
+        pass
+
+
+class Dispatcher:
+
+    def __init__(self):
+        pass
+
+    def add_tasks(self, path: str, details_csv: str) -> None:
+        if os.path.isdir(path):
+            if path[-1] not in ["/", "\\"]:
+                path += "/"
+            files = pd.DataFrame(columns=["filename", "basename"])
+            for i, filename in enumerate(glob.iglob(path + "**/**", recursive=True)):
+                if os.path.splitext(filename)[1] in SUPPORTED_FILETYPES:
+                    files.at[i] = [filename, os.path.basename(filename)]
+            files.drop_duplicates(inplace=True)
+            print(files)
+        else:
+            pass
         pass
